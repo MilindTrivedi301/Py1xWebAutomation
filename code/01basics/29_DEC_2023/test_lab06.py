9de39f430f43d5e46ef58204a7ce52386c843e0d
milind <milindtrivedi94@gmail.com>
2024-01-16 14:59:52 +0530

    commit from linux

--- code/01basics/29_DEC_2023/test_lab06.py
+++ code/01basics/29_DEC_2023/test_lab06.py
@@ -9,6 +9,21 @@ from selenium import webdriver
 
 def test_open_login():
     driver = webdriver.Chrome()
-    driver.get("https://app.vwo.com")
+    driver.get("https://www.google.com")
+
+    #driver.naigate().to x
+    print(driver.title)
+
+    driver.back()
+    driver.get("https://www.bing.com")
+    print(driver.title)
+
+    driver.forward()
+    print(driver.title)
+
+    driver.back()
+    print(driver.title)
+    driver.refresh()
+
     time.sleep(5)
     driver.quit()
